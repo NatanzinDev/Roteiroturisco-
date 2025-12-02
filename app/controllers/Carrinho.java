e52ae66b2d2acd603dbfb4797f5000808bdde34c
Natan Paulo <natanpaulo1300@gmail.com>
2025-12-02 16:09:38 -0300

    Adição e exclusão de itens do carrinho

--- app/controllers/Carrinho.java
+++ app/controllers/Carrinho.java
@@ -1,28 +1,79 @@
 package controllers;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import play.cache.Cache;
+import models.Compra;
+import models.ItemCompra;
+import models.PacoteTuristico;
+import models.StatusCompra;
+import models.Usuario;
 import play.mvc.Controller;
+import play.mvc.With;
 
-public class Carrinho extends Controller{
-	
+@With(Seguranca.class)
+public class Carrinho extends Controller {
+
 	public static void detalhes() {
-		render();
+
+		String email = session.get("usuario");
+
+		Usuario usuario = Usuario.find("email = ?1", email).first();
+
+		// Busca o carrinho ABERTO deste usuário
+		Compra carrinho = Compra.find("usuario = ?1 AND status = ?2", usuario, StatusCompra.CARRINHO).first();
+
+		render(carrinho);
+
+	}
+
+	public static void adicionarAjax(Long idPacote) {
+
+		String emailUsuario = session.get("usuario");
+
+		Usuario usuario = Usuario.find("email = ?1", emailUsuario).first();
+
+		Compra carrinho = Compra.find("usuario = ?1 AND status = ?2", usuario, StatusCompra.CARRINHO).first();
+
+		if (carrinho == null) {
+			carrinho = new Compra(usuario);
+			carrinho.save();
+		}
+
+		PacoteTuristico pacote = PacoteTuristico.findById(idPacote);
+
+		ItemCompra itemExistente = ItemCompra.find("compra = ?1 AND pacote = ?2", carrinho, pacote).first();
+
+		if (itemExistente != null) {
+			itemExistente.quantidade++;
+			itemExistente.save();
+		} else {
+			ItemCompra novoItem = new ItemCompra(pacote, carrinho);
+			novoItem.save();
+		}
+
+		Long totalItens = ItemCompra.count("compra = ?1", carrinho);
+
+		Map<String, Object> json = new HashMap<String, Object>();
+		json.put("total", totalItens);
+		json.put("mensagem", "Sucesso");
+
+		renderJSON(json);
 	}
 	
-	public static void adicionarAjax(Long idPacote) {
+	public static void remover(Long idItem) {
+
+	    ItemCompra item = ItemCompra.findById(idItem);
 	    
-	    List<Long> itens = Cache.get(session.getId() + "-carrinho", List.class);
-	    if(itens == null) {
-	    	itens = new ArrayList<Long>();
+	    if (item != null) {
+	        
+	        String email = session.get("usuario");
+	        if (item.compra.usuario.email.equals(email)) {
+	            item.delete();
+	            flash.success("Item removido com sucesso.");
+	        }
 	    }
 	    
-	    itens.add(idPacote);
-	    Cache.set(session.getId() + "-carrinho", itens);
-	    
-	    
-	    renderJSON(itens.size());
+	    detalhes();
 	}
 }
