eb4f5f73b17220003e2de630d0043f315cc3bf1c
Natan Paulo <natanpaulo1300@gmail.com>
2025-12-02 16:24:50 -0300

    Finalizando compra

--- app/controllers/Carrinho.java
+++ app/controllers/Carrinho.java
@@ -76,4 +76,49 @@ public class Carrinho extends Controller {
 	    
 	    detalhes();
 	}
+	
+	public static void finalizarCompra() {
+	    String email = session.get("usuario");
+	    Usuario usuario = Usuario.find("email = ?1", email).first();
+	    Compra carrinho = Compra.find("usuario = ?1 AND status = ?2", usuario, StatusCompra.CARRINHO).first();
+
+	   
+	    if (carrinho == null || carrinho.itens.size() == 0) {
+	        flash.error("Carrinho vazio!");
+	        detalhes();
+	    }
+
+	 
+	    String texto = "Olá! Gostaria de finalizar meu pedido no Explora Gostoso.\n\n";
+	    
+	    texto += "*Cliente:* " + usuario.nome + "\n";
+	    texto += "*Email:* " + usuario.email + "\n";
+	    texto += "------------------\n";
+
+	    
+	    for (ItemCompra item : carrinho.itens) {
+	        texto += "+ " + item.pacote.nome + " (Qtd: " + item.quantidade + ")\n";
+	    }
+	    
+	    texto += "\n*Total a Pagar:* R$ " + carrinho.getValorTotal();
+
+	    carrinho.status = StatusCompra.FINALIZADA;
+	    carrinho.save();
+
+	    try {
+	        // Transforma espaços e quebras de linha em códigos de URL (%20, %0A)
+	        String textoPronto = java.net.URLEncoder.encode(texto, "UTF-8");
+	        
+	        String telefone = "558491928201"; 
+	        
+	        String urlWhatsapp = "https://api.whatsapp.com/send?phone=" + telefone + "&text=" + textoPronto;
+	        
+	        redirect(urlWhatsapp);
+
+	    } catch (Exception e) {
+	        e.printStackTrace(); 
+	        flash.error("Erro ao gerar link do WhatsApp, mas seu pedido foi salvo!");
+	        detalhes();
+	    }
+	}
 }
